05e77603f901b28770f727ac97918e08d70e3611
Gianluca Benassi <benox1199@gmail.com>
2021-11-29 14:34:05 +0100

    js ToDoList array

--- js/script.js
+++ js/script.js
@@ -1,9 +1,32 @@
 const app = new Vue({
     el: '#root',
-    data: {
 
+    data: {
+        toDoList: [
+            {
+                text: 'Fare la spesa',
+                done: true
+            },
+            {
+                text: 'Pulire casa',
+                done: false
+            },
+            {
+                text: 'Fare gli esercizi di Boolean',
+                done: true
+            },
+            {
+                text: 'Cucinare la cena',
+                done: false
+            },
+            {
+                text: 'Finire una serie tv',
+                done: false
+            }
+        ]
     },
+
     methods:{
-        
+
     }
 });
